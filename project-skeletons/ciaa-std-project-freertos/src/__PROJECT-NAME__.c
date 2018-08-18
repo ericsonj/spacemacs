e93782690cb33aeaca5d2f5218a9333758530e45
Ericson Joseph <ericsonjoseph@gmail.com>
2018-08-17 19:29:57 -0500

    Add and fix projects

---
+++ project-skeletons/ciaa-std-project-freertos/src/__PROJECT-NAME__.c
@@ -0,0 +1,97 @@
+/* Copyright __YEAR__, __USER-NAME__
+ * All rights reserved.
+ *
+ * This file is part sAPI library for microcontrollers.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Date: __TIME__
+ */
+
+/*==================[inclusions]=============================================*/
+
+//#include "__PROJECT-NAME__.h"   // <= own header (optional)
+#include "sapi.h"       // <= sAPI header
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+void ledTask1(void *taskParmPtr);
+
+/*==================[external functions definition]==========================*/
+
+/* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void){
+
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   xTaskCreate(ledTask1,                 // FUNCTION
+               (const char *)"led1",     // TASK NAME
+               configMINIMAL_STACK_SIZE, // TASK STACK
+               NULL,                     // TASK ARGS
+               tskIDLE_PRIORITY + 1,     // TASK PRIORITY
+               NULL                      // SYSTEM TASK POINTER
+               );
+
+   // Iniciar scheduler
+   vTaskStartScheduler();
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while (TRUE) {
+   }
+
+   /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+      por ningun S.O. */
+   return 0;
+}
+
+void ledTask1(void *taskParmPtr) {
+    while (TRUE) {       
+        gpioToggle(LED1);
+        vTaskDelay(500 / portTICK_RATE_MS);
+    }
+}
+
+/*==================[end of file]============================================*/
