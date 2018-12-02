fc04a349feadc81424e224fbaefc6a596cf6ba0c
Ericson Joseph <ericsonjoseph@gmail.com>
2018-12-01 19:05:34 -0500

    Add ice40-hx8k project

---
+++ project-skeletons/ice40-hx8k-project/__PROJECT-NAME__.v
@@ -0,0 +1,9 @@
+/**
+ * File:   __PROJECT-NAME__.v
+ * Author: __USER-NAME__
+ * 
+ * Created on __(format-time-string "%B %e, %Y, %l:%M %p")__
+ */
+
+module __PROJECT-NAME__ (/*PORTS*/);
+endmodule // __PROJECT-NAME__
---
+++ project-skeletons/ice40-hx8k-project/__PROJECT-NAME___tb.v
@@ -0,0 +1,9 @@
+/**
+ * File:   __PROJECT-NAME___tb.v
+ * Author: __USER-NAME__
+ * 
+ * Created on __(format-time-string "%B %e, %Y, %l:%M %p")__
+ */
+
+module __PROJECT-NAME___tb (/**PORTS**/);
+endmodule // __PROJECT-NAME___tb
